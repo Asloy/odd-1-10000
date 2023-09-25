710bc88962c7b179d3fc307444113d4c67b89aba
Asloy <145319529+Asloy@users.noreply.github.com>
2023-09-25 13:54:47 +0800

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg10000.odd;
+
+/**
+ *
+ * @author Emo_CPE
+ */
+public class Odd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         int x=1;
+       while (x<=10000){   
+           System .out.println(x+" ");
+           x=x+2;
+           
+       }
+    }
+    
+}
